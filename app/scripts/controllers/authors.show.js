729c13f6b7fd6482b229c0066a430673f3f11ba2
Yaser Almasri <yaserms@outlook.com>
2016-04-07 09:31:53 -0500

    modify author show ctrl to get list of author books

--- app/scripts/controllers/authors.show.js
+++ app/scripts/controllers/authors.show.js
@@ -4,16 +4,32 @@ angular
   .controller('AuthorsShowCtrl', [
     '$scope',
     'Author',
+    'Book',
     '$location',
     '$routeParams',
-    function($scope, Author, $location, $routeParams){
+    function($scope, Author, Book, $location, $routeParams){
       $scope.author = Author.get({ id: $routeParams.id });
+      $scope.books = Book.query({ author_id: $routeParams.id });
 
       $scope.delete = function(author){
         if(confirm('Are you sure?')){
           var author = new Author(author);
           author.$delete(function(){
             $location.path('/authors');
+          });
+        }
+      }
+
+      $scope.deleteBook = function(book){
+        if(confirm('Are you sure?')){
+          var book = new Book(book);
+          book.$delete(function(){
+            $scope.books.forEach(function(b, index){
+              if(book.id == b.id){
+                $scope.books.splice(index, 1);
+                return false;
+              }
+            });
           });
         }
       }
