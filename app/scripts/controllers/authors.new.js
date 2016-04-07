c7a76627e2cb14a5f94fc5d7383e647b7e0a7860
Yaser Almasri <yaserms@outlook.com>
2016-04-06 22:36:26 -0500

    add authors new controller

---
+++ app/scripts/controllers/authors.new.js
@@ -0,0 +1,18 @@
+angular
+  .module('maSysPayApp')
+  .controller('AuthorsNewCtrl', [
+    '$scope',
+    'Author',
+    '$location',
+    function($scope, Author, $location){
+      $scope.author = new Author();
+
+      $scope.create = function(author){
+        author.$save(function(res){
+          $location.path('/authors');
+        }, function(res){
+          alert(JSON.stringify(res));
+        }
+      }
+    }
+  ])
